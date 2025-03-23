ac39835a645269f04d0ac8fe9fdbd40883cce5c0
cheolkyu Lee <cheolkyulee@cheolkyus-MBP.lm4837>
2025-03-23 14:47:47 +0900

    first commit

---
+++ backend/requestboard/src/main/java/com/ktds/azure/tenant/requestboard/RequestboardApplication.java
@@ -0,0 +1,13 @@
+package com.ktds.azure.tenant.requestboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequestboardApplication.class, args);
+	}
+
+}
---
+++ backend/requestboard/src/test/java/com/ktds/azure/tenant/requestboard/RequestboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktds.azure.tenant.requestboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
