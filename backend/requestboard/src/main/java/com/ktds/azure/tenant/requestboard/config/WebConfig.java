666b121c97be5ed669de74be541f0984cddcb708
midi79 <midi79@naver.com>
2025-04-16 10:13:38 +0900

    Fix 404 error and modify db url and password

--- backend/requestboard/src/main/java/com/ktds/azure/tenant/requestboard/config/WebConfig.java
+++ backend/requestboard/src/main/java/com/ktds/azure/tenant/requestboard/config/WebConfig.java
@@ -14,10 +14,5 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedHeaders("*")
                 .allowCredentials(true);
 
-        registry.addMapping("/qna/api/**")
-                .allowedOrigins("http://localhost:3000", "https://atportal.cbiz.kubepia.net", "https://atportal-dev.cbiz.kubepia.net")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
-                .allowedHeaders("*")
-                .allowCredentials(true);
     }
 }
